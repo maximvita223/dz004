e8296b695d9d15b6e1e72f7a973df82c89416a7b
Maxim <maks.bidenko.96@mail.ru>
2022-10-05 20:08:18 +0300

    Сделал 2 задачу

---
+++ ex002.py
@@ -0,0 +1,13 @@
+# Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+n = int(input('Введите число - '))
+spi = []
+i = n
+while n!=1:
+    for j in range(2,100):
+        if not n%j:
+            n = n/j
+            spi.append(j)
+if spi[0]==i:
+    spi.append(1)         
+print(spi)
