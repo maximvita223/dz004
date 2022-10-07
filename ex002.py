938b3be15ed794407bdafba162dd8efbfd22cf04
maximvita223 <109244209+maximvita223@users.noreply.github.com>
2022-10-07 08:31:08 +0300

    Update ex002.py
    fixed

--- ex002.py
+++ ex002.py
@@ -5,9 +5,10 @@ spi = []
 i = n
 while n!=1:
     for j in range(2,100):
-        if not n%j:
-            n = n/j
-            spi.append(j)
+        for k in range(2,100):
+            if not n%j:
+                n = n//j
+                spi.append(j)
 if spi[0]==i:
     spi.append(1)         
 print(spi)
